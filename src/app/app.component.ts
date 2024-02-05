a02e19e2d5070fa3cee60933194ea4849885c02d
Jesus Lopez <sonnama.32@gmail.com>
2024-02-04 20:38:38 -0600

    Primer commit para el frontend de la tienda de cursos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+declare function HOMEINIT([]):any;
+declare var $:any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cusosApp';
+  title = 'cursosApp';
+
+  ngOnInit():void{
+
+    setTimeout(() => {
+      HOMEINIT($);
+    }, 50);
+  }
 }
